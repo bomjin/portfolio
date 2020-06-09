cf6a01913323c474eb484231e34af876de13bda3
bomjin <47442949+bomjin@users.noreply.github.com>
2020-06-09 16:34:50 +0900

    JS :  filter

--- main.js
+++ main.js
@@ -49,6 +49,30 @@ document.addEventListener('scroll', () => {
 arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
+
+//projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add('anim-out');
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisble');
+      } else {
+        project.classList.add('invisble');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+});
+
 //scrollIntoView를 따로 생성
 function scrollIntoView(selectior) {
   const scrollTo = document.querySelector(selectior);
